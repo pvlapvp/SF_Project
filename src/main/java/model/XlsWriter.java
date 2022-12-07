94ddf73ea449df95ed05ca9096c23b570eaa219e
pavel <pvlapin@mail.ru>
2022-12-07 22:01:38 +0700

    сделана генерация заголовка со стилями и запись в файл, пока на этом всё, хочется спать

--- src/main/java/model/XlsWriter.java
+++ src/main/java/model/XlsWriter.java
@@ -1,6 +1,14 @@
 package model;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class XlsWriter {
@@ -12,7 +20,41 @@ public class XlsWriter {
     TODO: Также в этом методе реализовать заполнение строк таблицы данными, хранящимися в коллекции элементов Statistics
     TODO: После генерации в этом же методе файл необходимо создать (используя FileOutputStream), задав ему полученное на вход метода имя
      */
-    public void generateXlsTable(List<Statistics> statisticsList, File file){
+    public static void generateXlsTable(List<Statistics> statisticsList, String fileName) throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("statistics");
 
+        Font font = workbook.createFont();
+        font.setBold(true);
+        font.setFontHeight((short) 280);
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setFont(font);
+
+        Row row = sheet.createRow(0);
+        String[] strings = new String[] {"studyProfile", "avgExamScore", "profileStudentsAmount", "universityName"};
+
+        int i = 0;
+        for (String s: strings) {
+            Cell cell = row.createCell(i);
+            cell.setCellValue(s);
+            cell.setCellStyle(cellStyle);
+            sheet.autoSizeColumn(i);
+            i++;
+        }
+
+        workbook.write(new FileOutputStream(fileName));
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<Statistics> statisticsList = new ArrayList<>();
+        statisticsList.add(new Statistics("a", 4, 40, "aa"));
+        statisticsList.add(new Statistics("b", 3, 50, "bb"));
+        statisticsList.add(new Statistics("c", 2, 67, "cc"));
+        statisticsList.add(new Statistics("d", 5, 45, "dd"));
+        statisticsList.add(new Statistics("e", 4, 67, "ee"));
+        statisticsList.add(new Statistics("f", 3, 34, "ff"));
+        statisticsList.add(new Statistics("g", 2, 55, "gg"));
+
+        generateXlsTable(statisticsList, "e:/1.xlsx");
     }
 }
